e5440b3e0083325d897622f0101f9e8d2557cd36
Alejandro <alejandrochang10@gmail.com>
2018-12-06 14:05:13 -0800

    component refactoring

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position),
-    (error) => console.log(error)
-  );
-
-
-  return (
-    <div>Hello World</div>
-  );
+class App extends React.Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => console.log(position),
+      (error) => console.log(error),
+    );
+    return(
+      <div>Hello World</div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
