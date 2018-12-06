a99779582e637cb8c4d392ea35537d46967a0c16
Alejandro <alejandrochang10@gmail.com>
2018-12-06 15:06:08 -0800

    conditional rendering

--- src/index.js
+++ src/index.js
@@ -3,15 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class App extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = { lat: null, lon: null, errorMessage: ''};
     window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
-      (error) => console.log(error),
-    );
-    return(
-      <div>Hello World</div>
+      (position) => {
+        this.setState({ lat: position.coords.latitude, lon: position.coords.longitude });
+      },
+      (error) => {
+        this.setState({ errorMessage: error.message });
+      },
     );
   }
+
+  render() {
+    if (this.state.errorMessage && !this.state.lat && !this.state.lon) {
+      return <div>Error: {this.state.errorMessage}</div>
+    } else if (!this.state.errorMessage && !this.state.lat || !this.state.lon) {
+      return <div>...loading</div>
+    } else {
+      return <div>Your current latitude and longitude is [lat: {this.state.lat}, lon: {this.state.lon}]</div>
+    } 
+  };
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
