21055b8d34bdaa4929f04fe5a45dfdb9f92102b5
Alejandro <alejandrochang10@gmail.com>
2018-12-06 13:32:13 -0800

    getting users physical location

--- src/index.js
+++ src/index.js
@@ -2,5 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+const App = () => {
+  window.navigator.geolocation.getCurrentPosition(
+    (position) => console.log(position),
+    (error) => console.log(error)
+  );
+
+
+  return (
+    <div>Hello World</div>
+  );
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
